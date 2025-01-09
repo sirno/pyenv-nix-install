c501f2281aeaa04da190724e02db878050e59e43
Nicolas Ochsner <nicolasochsner@gmail.com>
2025-01-09 20:06:47 +0100

    Add nixpkgs-python to flake inputs

--- flake.nix
+++ flake.nix
@@ -1,20 +1,36 @@
 {
   description = "pyenv plugin to manage python versions with nix";
 
-  inputs.nixpkgs.url = "nixpkgs";
-  inputs.flake-utils.url = "github:numtide/flake-utils";
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-24.11";
 
-  outputs = { self, nixpkgs, flake-utils }:
-    flake-utils.lib.eachDefaultSystem (system:
+    flake-utils.url = "github:numtide/flake-utils";
+
+    nixpkgs-python.url = "github:cachix/nixpkgs-python";
+  };
+
+  outputs =
+    {
+      self,
+      nixpkgs,
+      flake-utils,
+      nixpkgs-python,
+    }:
+    flake-utils.lib.eachDefaultSystem (
+      system:
       let
         pkgs = nixpkgs.legacyPackages.${system};
         pyenv = pkgs.pyenv;
-        pyenv-nix-install = pkgs.callPackage ./default.nix {};
-      in {
+        pyenv-nix-install = pkgs.callPackage ./default.nix { };
+      in
+      {
         packages.default = pyenv-nix-install;
 
         devShells.default = pkgs.mkShell {
-          buildInputs = [ pyenv pyenv-nix-install ];
+          buildInputs = [
+            pyenv
+            pyenv-nix-install
+          ];
 
           shellHook = ''
             export PYENV_ROOT=${pyenv}
